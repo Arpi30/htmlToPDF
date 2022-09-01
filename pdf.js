f31b12ab949713bae2c50be464626a546e870e08
Ács Árpád <alijie24@gmail.com>
2022-09-01 17:24:37 +0200

    filesaver library and events

--- pdf.js
+++ pdf.js
@@ -1,15 +1,65 @@
-const invoice = document.getElementById("invoice")
-const button = document.getElementById("download")
+const form = document.getElementById("form")
+const commentsDIV = document.getElementById("comments")
+const pdfCheckBox = document.getElementById("pdfCheckbox")
+const txtCheckBox = document.getElementById("txtCheckbox")
+const saveButton = document.getElementById("saveButton")
 
-let opt = {
-    margin:       0,
-    filename:     'myfile.pdf',
-    image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 2 },
-    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait'},
+let state = []
+
+form.addEventListener('keypress',(e) => {
+  if(e.key === 'Enter'){
+    e.preventDefault()
+    const value = form['value'].value
+    if(!value){
+      alert('Please type something')
+    }
+    state.push({value})
+    form['value'].value = ''
+    eventFunction.render()
   }
+})
 
-button.onclick = () => {
-    html2pdf().from(invoice).set(opt).save();
+saveButton.onclick = (e) => {
+  e.preventDefault()
+  if(pdfCheckBox.checked){
+    eventFunction.toPdf()
+  }else if(txtCheckBox.checked){
+    eventFunction.toTxt()
+  } else{
+    alert('I cant download anything if you dont choose')
+  }
 }
+
+
+
+const eventFunction = {
+  render: () => {
+    let comments = ''
+    state.forEach( comment => {
+      comments += `
+        <div>${comment.value}</div>
+      `
+    })
+    commentsDIV.innerHTML = comments
+  },
+  toPdf: () => {
+    let opt = {
+      margin:       0,
+      filename:     'myfile.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait'},
+    }
+    html2pdf().from(commentsDIV).set(opt).save();
+  },
+
+  toTxt: () => {
+    let blob = new Blob([JSON.stringify(state)], {type:'text/plain'})
+    saveAs(blob, 'mentes')
+  }
+}
+
+
+
+
         
