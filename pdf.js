62146d8fb3bba87ffa4f75a4ac69e7b92aa8f1ac
Ács Árpád <alijie24@gmail.com>
2022-09-01 20:56:46 +0200

    create obj

--- pdf.js
+++ pdf.js
@@ -1,65 +1,61 @@
-const form = document.getElementById("form")
-const commentsDIV = document.getElementById("comments")
-const pdfCheckBox = document.getElementById("pdfCheckbox")
-const txtCheckBox = document.getElementById("txtCheckbox")
-const saveButton = document.getElementById("saveButton")
 
-let state = []
+const obj = {
+  state: [],
+  eventFunction: {
+    render: () => {
+      let comments = ''
+      obj.state.forEach( comment => {
+        comments += `
+          <div>${comment.value}</div>
+        `
+      })
+      obj.htmlTag.commentsDIV.innerHTML = comments
+    },
+    toPdf: () => {
+      let opt = {
+        margin:       0,
+        filename:     'myfile.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait'},
+      }
+      html2pdf().from(obj.htmlTag.commentsDIV).set(opt).save();
+    },
 
-form.addEventListener('keypress',(e) => {
+    toTxt: () => {
+      let blob = new Blob([JSON.stringify(obj.state)], {type:'text/plain'})
+      saveAs(blob, 'mentes')
+    }
+  },
+  htmlTag:{
+    form: document.getElementById("form"),
+    commentsDIV: document.getElementById("comments"),
+    pdfCheckBox: document.getElementById("pdfCheckbox"),
+    txtCheckBox: document.getElementById("txtCheckbox"),
+    saveButton: document.getElementById("saveButton"),
+  }
+}
+
+obj.htmlTag.form.addEventListener('keypress',(e) => {
   if(e.key === 'Enter'){
     e.preventDefault()
-    const value = form['value'].value
+    const value = obj.htmlTag.form['value'].value
     if(!value){
       alert('Please type something')
     }
-    state.push({value})
-    form['value'].value = ''
-    eventFunction.render()
+    obj.state.push({value})
+    obj.htmlTag.form['value'].value = ''
+    obj.eventFunction.render()
   }
 })
 
-saveButton.onclick = (e) => {
+obj.htmlTag.saveButton.onclick = (e) => {
   e.preventDefault()
-  if(pdfCheckBox.checked){
-    eventFunction.toPdf()
-  }else if(txtCheckBox.checked){
-    eventFunction.toTxt()
+  if(obj.htmlTag.pdfCheckBox.checked){
+    obj.eventFunction.toPdf()
+  }else if(obj.htmlTag.txtCheckBox.checked){
+    obj.eventFunction.toTxt()
   } else{
     alert('I cant download anything if you dont choose')
   }
 }
-
-
-
-const eventFunction = {
-  render: () => {
-    let comments = ''
-    state.forEach( comment => {
-      comments += `
-        <div>${comment.value}</div>
-      `
-    })
-    commentsDIV.innerHTML = comments
-  },
-  toPdf: () => {
-    let opt = {
-      margin:       0,
-      filename:     'myfile.pdf',
-      image:        { type: 'jpeg', quality: 0.98 },
-      html2canvas:  { scale: 2 },
-      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait'},
-    }
-    html2pdf().from(commentsDIV).set(opt).save();
-  },
-
-  toTxt: () => {
-    let blob = new Blob([JSON.stringify(state)], {type:'text/plain'})
-    saveAs(blob, 'mentes')
-  }
-}
-
-
-
-
-        
