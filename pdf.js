39415b7aea9b812d8de17a8c21dcd837988b85fb
Ács Árpád <alijie24@gmail.com>
2022-09-02 14:27:40 +0200

    prettier

--- pdf.js
+++ pdf.js
@@ -1,88 +1,86 @@
-
 const obj = {
   state: [],
   eventFunction: {
     render: () => {
-      let comments = ''
-      obj.state.forEach( comment => {
+      let comments = "";
+      obj.state.forEach((comment) => {
         comments += `
           <div>${comment.value}</div>
-        `
-      })
-      obj.htmlTag.commentsDIV.innerHTML = comments
+        `;
+      });
+      obj.htmlTag.commentsDIV.innerHTML = comments;
     },
     toPdf: () => {
       let opt = {
-        margin:       0,
-        filename:     'myfile.pdf',
-        image:        { type: 'jpeg', quality: 0.98 },
-        html2canvas:  { scale: 2 },
-        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait'},
-      }
+        margin: 0,
+        filename: "myfile.pdf",
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+      };
       html2pdf().from(obj.htmlTag.commentsDIV).set(opt).save();
     },
 
     toTxt: () => {
-      let blob = new Blob([JSON.stringify(obj.state)], {type:'text/plain'})
-      saveAs(blob, 'mentes')
+      let blob = new Blob([JSON.stringify(obj.state)], { type: "text/plain" });
+      saveAs(blob, "mentes");
     },
     writeToWindow: () => {
-      obj.htmlTag.form.addEventListener('keypress',(e) => {
-        if(e.key === 'Enter'){
-          e.preventDefault()
-          const value = obj.htmlTag.form['value'].value
-          if(!value){
-            alert('Please type something')
+      obj.htmlTag.form.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          const value = obj.htmlTag.form["value"].value;
+          if (!value) {
+            alert("Please type something");
           }
-          obj.state.push({value})
-          obj.htmlTag.form['value'].value = ''
-          obj.eventFunction.render()
+          obj.state.push({ value });
+          obj.htmlTag.form["value"].value = "";
+          obj.eventFunction.render();
         }
-      })
-    }
+      });
+    },
   },
   downloadProcess: () => {
-    obj.htmlTag.boxes.forEach(box => {
+    obj.htmlTag.boxes.forEach((box) => {
       box.onclick = () => {
-        if(obj.htmlTag.pdfCheckBox.checked){
-          obj.htmlTag.txtCheckBox.disabled = obj.htmlTag.pdfCheckBox.checked
+        if (obj.htmlTag.pdfCheckBox.checked) {
+          obj.htmlTag.txtCheckBox.disabled = true;
           obj.htmlTag.saveButton.onclick = (e) => {
-            e.preventDefault()
-            obj.eventFunction.toPdf()
-            obj.htmlTag.pdfCheckBox.checked = !obj.htmlTag.pdfCheckBox.checked
-            obj.htmlTag.txtCheckBox.disabled = !obj.htmlTag.pdfCheckBox.checked
-          }
-        }else{
-          obj.htmlTag.txtCheckBox.disabled = false
+            e.preventDefault();
+            obj.eventFunction.toPdf();
+            obj.htmlTag.pdfCheckBox.checked = false;
+            obj.htmlTag.txtCheckBox.disabled = false;
+          };
+        } else {
+          obj.htmlTag.txtCheckBox.disabled = false;
         }
-    
-        if (obj.htmlTag.txtCheckBox.checked){
-          obj.htmlTag.pdfCheckBox.disabled = obj.htmlTag.txtCheckBox.checked
+
+        if (obj.htmlTag.txtCheckBox.checked) {
+          obj.htmlTag.pdfCheckBox.disabled = true;
           obj.htmlTag.saveButton.onclick = (e) => {
-            e.preventDefault()
-            obj.eventFunction.toTxt()
-            obj.htmlTag.txtCheckBox.checked = !obj.htmlTag.txtCheckBox.checked
-            obj.htmlTag.pdfCheckBox.disabled = !obj.htmlTag.txtCheckBox.checked
-          }
-        }else{
-          obj.htmlTag.pdfCheckBox.disabled = false
+            e.preventDefault();
+            obj.eventFunction.toTxt();
+            obj.htmlTag.txtCheckBox.checked = false;
+            obj.htmlTag.pdfCheckBox.disabled = false;
+          };
+        } else {
+          obj.htmlTag.pdfCheckBox.disabled = false;
         }
-      }
-    })
+      };
+    });
   },
-  htmlTag:{
+  htmlTag: {
     form: document.getElementById("form"),
     commentsDIV: document.getElementById("comments"),
     pdfCheckBox: document.getElementById("pdfCheckbox"),
     txtCheckBox: document.getElementById("txtCheckbox"),
     saveButton: document.getElementById("saveButton"),
-    boxes: document.querySelectorAll('.checkbox')
+    boxes: document.querySelectorAll(".checkbox"),
   },
   callFunction: () => {
-    obj.downloadProcess()
-    obj.eventFunction.writeToWindow()
+    obj.downloadProcess();
+    obj.eventFunction.writeToWindow();
+  },
+};
 
-  }
-}
-
-obj.callFunction()
+obj.callFunction();
